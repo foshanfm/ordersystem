bee6b0fc10e1dc6368f62894a16f187eebfd56cc
bombe <fungkinpong2013@gmail.com>
2017-11-27 14:13:04 +0000

    Finish layout and routes

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -11,4 +11,14 @@ class StaticPagesController extends Controller
     {
         return view('static_pages/home');
     }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
 }
